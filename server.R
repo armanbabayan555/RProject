02e143278d4ec1d74726f4de78c2257cf0cdf1e1
armen-akyan <armen.armenakyan@picsart.com>
2022-08-06 13:57:17 +0400

    Added basic graph type choosing functionality

--- server.R
+++ server.R
@@ -2,7 +2,20 @@ library(DT)
 library(shiny)
 library(ggplot2)
 
-draw_plot <- function(data_input, num_var_1, num_var_2, fact_var) {
+get_graph_type <- function(graph_type) {
+  if (graph_type == "point") {
+    return(geom_point())
+  }
+  else if (graph_type == "bar") {
+    return(geom_bar(stat = "identity"))
+  }
+  else if (graph_type == "hist") {
+    return(geom_histogram(stat="count"))
+  }
+}
+
+
+draw_plot <- function(data_input, num_var_1, num_var_2, fact_var, graph_type) {
   if (num_var_1 != not_sel & !is.numeric(num_var_1)) {
     data_input[, (num_var_1)] <- as.factor(data_input[, get(num_var_1)])
   }
@@ -18,49 +31,49 @@ draw_plot <- function(data_input, num_var_1, num_var_2, fact_var) {
     fact_var != not_sel) {
     ggplot(data = data_input,
            aes_string(x = num_var_1, y = num_var_2, color = fact_var)) +
-      geom_point()
+      get_graph_type(graph_type)
   }
   else if (num_var_1 != not_sel &
     num_var_2 != not_sel &
     fact_var == not_sel) {
     ggplot(data = data_input,
            aes_string(x = num_var_1, y = num_var_2)) +
-      geom_point()
+      get_graph_type(graph_type)
   }
   else if (num_var_1 != not_sel &
     num_var_2 == not_sel &
     fact_var != not_sel) {
     ggplot(data = data_input,
            aes_string(x = fact_var, y = num_var_1)) +
-      geom_violin()
+      get_graph_type(graph_type)
   }
   else if (num_var_1 == not_sel &
     num_var_2 != not_sel &
     fact_var != not_sel) {
     ggplot(data = data_input,
            aes_string(x = fact_var, y = num_var_2)) +
-      geom_violin()
+      get_graph_type(graph_type)
   }
   else if (num_var_1 != not_sel &
     num_var_2 == not_sel &
     fact_var == not_sel) {
     ggplot(data = data_input,
            aes_string(x = num_var_1)) +
-      geom_histogram()
+      get_graph_type(graph_type)
   }
   else if (num_var_1 == not_sel &
     num_var_2 != not_sel &
     fact_var == not_sel) {
     ggplot(data = data_input,
            aes_string(x = num_var_2)) +
-      geom_histogram()
+      get_graph_type(graph_type)
   }
   else if (num_var_1 == not_sel &
     num_var_2 == not_sel &
     fact_var != not_sel) {
     ggplot(data = data_input,
            aes_string(x = fact_var)) +
-      geom_bar()
+      get_graph_type(graph_type)
   }
 }
 
@@ -100,9 +113,10 @@ server <- function(input, output, session) {
 
   first_var_1 <- eventReactive(input$run_button_1, input$first_var_1)
   first_var_2 <- eventReactive(input$run_button_1, input$first_var_2)
+  graph_type_1 <- eventReactive(input$run_button_1, input$graph_type_1)
 
   plot_1 <- eventReactive(input$run_button_1, {
-    draw_plot(getData(), first_var_1(), first_var_2(), not_sel)
+    draw_plot(getData(), first_var_1(), first_var_2(), not_sel, graph_type_1())
   })
 
   output$plot_1 <- renderPlot(plot_1())
@@ -124,9 +138,11 @@ server <- function(input, output, session) {
 
   second_var_1 <- eventReactive(input$run_button_2, input$second_var_1)
   second_var_2 <- eventReactive(input$run_button_2, input$second_var_2)
+  graph_type_2 <- eventReactive(input$run_button_1, input$graph_type_2)
+
 
   plot_2 <- eventReactive(input$run_button_2, {
-    draw_plot(getData(), second_var_1(), second_var_2(), not_sel)
+    draw_plot(getData(), second_var_1(), second_var_2(), not_sel, graph_type_2())
   })
 
   output$plot_2 <- renderPlot(plot_2())
@@ -150,9 +166,11 @@ server <- function(input, output, session) {
   third_var_1 <- eventReactive(input$run_button_3, input$third_var_1)
   third_var_2 <- eventReactive(input$run_button_3, input$third_var_2)
   third_var_3 <- eventReactive(input$run_button_3, input$third_var_3)
+  graph_type_3 <- eventReactive(input$run_button_1, input$graph_type_3)
+
 
   plot_3 <- eventReactive(input$run_button_3, {
-    draw_plot(getData(), third_var_1(), third_var_2(), third_var_3())
+    draw_plot(getData(), third_var_1(), third_var_2(), third_var_3(), graph_type_3())
   })
 
   output$plot_3 <- renderPlot(plot_3())
